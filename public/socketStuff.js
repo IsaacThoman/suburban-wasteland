761df4e949fdc630b4c9e15b18a7c8e7f849ebdb
IsaacThoman <isaacthoman@outlook.com>
2022-01-29 14:38:13 -0500

    doesn't allow shooting through walls

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -19,8 +19,11 @@ let lastShot = 0;
 function cKeyPressed(){
     if(utcTime>lastShot+1){
         for(let i = 0; i<objectsToRender.length; i++){
-            if(objects[objectsToRender[i]]['type']=='remotePlayer' &&playerPointedAt == objects[objectsToRender[i]]['playerNum']&& Math.abs(objects[objectsToRender[i]]['dirDiff'])<0.1)
+            if(objects[objectsToRender[i]]['type']=='remotePlayer' &&playerPointedAt == objects[objectsToRender[i]]['playerNum']&& Math.abs(objects[objectsToRender[i]]['dirDiff'])<0.1){
+                if(wallBetween(objects[objectsToRender[i]],localPlayer))
                 socket.emit('playerShot', objects[objectsToRender[i]]['playerNum']);
+            }
+
         }
 
         lastShot = utcTime;
